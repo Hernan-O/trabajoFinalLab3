d3a19797e94c9d313ba9ad35fc2766e4ba2a1d66
Hernán Navarra <101644564+Hernan-O@users.noreply.github.com>
2023-06-16 12:02:58 -0300

    Creacion de Stat random para el algoritmo de victoria

--- src/clases/Caballo.java
+++ src/clases/Caballo.java
@@ -1,15 +1,22 @@
-package clases;
+package trabajao;
+
+import static java.lang.Math.random;
 
 public class Caballo {
     private int edad;
     private String nombre;
     private Jockey piloto;
     private String raza;
+    private float stat; //Le genera un numero al azar con el que se mide la posibilidad de ganar
 
-    public Caballo(int edad,String nombre,Jockey piloto){
+    public Caballo(int edad,String nombre,Jockey piloto,String raza) {
         this.edad = edad;
         this.nombre = nombre;
         this.piloto = piloto;
+        if (raza.equalsIgnoreCase("Arabe") || raza.equalsIgnoreCase("Anglo") || raza.equalsIgnoreCase("Americano")) {
+            this.raza = raza;
+        }
+        this.stat =(float)(Math.random()*10+1);
     }
     public int getEdad() {
         return edad;
@@ -34,7 +41,7 @@ public class Caballo {
     public void setRaza(String raza) {
         this.raza = raza;
     }
-    
+
     public Jockey getPiloto() {
         return piloto;
     }
@@ -42,10 +49,15 @@ public class Caballo {
     public void setPiloto(Jockey piloto) {
         this.piloto = piloto;
     }
-   public void mostrarInformacion(){
+    public void setStat(){
+        this.stat =(float)(Math.random()*10+1);
+    }
+    public float getStat(){
+        return this.stat;
+    }
+    public void mostrarInformacion(){
         System.out.println("Nombre del caballo:"+this.getNombre()+"\nEdad caballo"+this.getEdad());
         this.piloto.mostrarInformacion();
     }
-    
 
 }
