6f54399d40037ef8cd837a8cd6fc81ba4b50df2a
FelipeMorgade <100422597+FelipeMorgade@users.noreply.github.com>
2023-06-16 12:58:11 -0300

    Add files via upload
    Agrego while - Switch y implementancion de excepciones.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int continuar = 0;
+        while (continuar == 0) {
+            try {
+                Scanner teclado = new Scanner(System.in);
 
+                System.out.println("Ingrese opcion:");
+                System.out.println("1: JUGAR");
+                System.out.println("2: SALIR");
 
+                int op = teclado.nextInt();
+                switch (op) {
+                    case 1:
+                        System.out.println("Opcion 1");
+                        break;
 
+                    case 2:
+                        System.out.println("Opcion 2");
+                        break;
 
+                    default:
+                        System.out.println("Ingrese una opcion correcta.");
+                        break;
+                }
 
+            } catch (InputMismatchException e) {
+                //System.out.println(e.getMessage());
+                //e.printStackTrace();
+                System.out.println("Ingrese una opcion correcta");
+            }
+        }
     }
 }
